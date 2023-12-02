3b2f8309d1af89d4146682a168080da90c162389
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-02 12:12:46 +0000

    Sat, Dec 2, 2023, 5:12 PM +05:00

---
+++ readability/readability.c
@@ -0,0 +1,95 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // Coleman-Liau Index formula
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+
+    else
+    {
+        printf("Grade %i\n", (int) round(index));
+    }
+
+}
+
+int count_letters(string text)
+{
+    int countl = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i]))
+        {
+            countl++;
+        }
+
+        else
+        {
+            continue;
+        }
+    }
+
+    return countl;
+}
+
+int count_words(string text)
+{
+    int countw = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isspace(text[i]))
+        {
+            countw++;
+        }
+
+        else
+        {
+            continue;
+        }
+    }
+
+    return countw + 1;
+}
+
+int count_sentences(string text)
+{
+    int counts = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!' )
+        {
+            counts++;
+        }
+
+        else
+        {
+            continue;
+        }
+    }
+    return counts;
+}
