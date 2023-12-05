7c88f29ff51ac8cdab59f5ce54f516ea2f312b03
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-05 09:39:14 +0000

    Tue, Dec 5, 2023, 2:39 PM +05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,6 +71,7 @@ bool vote(string name)
         if (strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes++;
+            return true;
         }
     }
     return false;
