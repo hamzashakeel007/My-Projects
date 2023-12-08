5741cc3f15f81bacce088b72ed571c9357267933
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-08 13:03:35 +0000

    Fri, Dec 8, 2023, 6:03 PM +05:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,7 +7,10 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
     {
-        if (image[i][j]rgnt == 0x00 &&)
+        if (image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00 && image[i][j].rgbtRed == 0x00)
+        {
+            
+        }
     }
     }
 }
