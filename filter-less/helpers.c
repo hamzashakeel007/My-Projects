cb3e5ca04531ed0cabf489013a9f752692c5f151
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-09 14:15:07 +0000

    Sat, Dec 9, 2023, 7:15 PM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,11 +1,11 @@
 #include "helpers.h"
-#include <math.h>
+#include "math.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     int gf;
-    
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
