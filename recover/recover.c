e2977404fcb9eafdc24013a8f839c969e94b84fc
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-09 20:54:02 +0000

    Sun, Dec 10, 2023, 1:54 AM +05:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Invalid commmand line argument. Must only be 2 line argument\n Usage: ./recover IMAGE\n");
-        return 0;
+        return 1;
     }
 
     FILE *infile = fopen(argv[1], "r");
