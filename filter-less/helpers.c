3d2e4fe56f5bdaacc3024bc994407bfc8187a110
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-09 14:47:56 +0000

    Sat, Dec 9, 2023, 7:47 PM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -36,9 +36,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             originalBlue = image[i][j].rgbtBlue;
 
             // Sepia filter formula
-            int sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
-            int sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
-            int sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
 
             if (sepiaRed > 255)
             {
