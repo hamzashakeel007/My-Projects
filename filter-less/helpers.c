49404eafec7b4ef2b709636f5939f5263c63bb34
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-09 14:55:00 +0000

    Sat, Dec 9, 2023, 7:55 PM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -71,11 +71,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // Averaging for gray filter
-            grayfilter = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
-            image[i][j].rgbtGreen = grayfilter;
-            image[i][j].rgbtRed = grayfilter;
-            image[i][j].rgbtBlue = grayfilter;
         }
     }
     return;
