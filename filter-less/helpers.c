fb7cfa51a6d5e824cc24d4a8876b8192ed4cc0f9
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-09 14:52:43 +0000

    Sat, Dec 9, 2023, 7:52 PM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -67,6 +67,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Averaging for gray filter
+            grayfilter = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtGreen = grayfilter;
+            image[i][j].rgbtRed = grayfilter;
+            image[i][j].rgbtBlue = grayfilter;
+        }
+    }
     return;
 }
 
