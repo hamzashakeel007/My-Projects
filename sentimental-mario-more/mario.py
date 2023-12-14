694453feeb83131d1e22b40dba06b1b19542eec3
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-14 12:36:32 +0000

    Thu, Dec 14, 2023, 5:36 PM +05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,40 @@
 # TODO
+#include <cs50.h>
+#include <stdio.h>
+
+void pyramid(void);
+
+int main(void)
+{
+    //Algorthm for building pyramid
+    pyramid();
+}
+
+
+
+void pyramid(void)
+{
+    // Pyramid height input
+    int height = 0;
+
+    while ( (height < 1) || (height > 8) )
+    {
+        height = get_int("Enter height for mario's pyramid: \n");
+    }
+
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < height+i+3; ++j)
+        {
+            if ( (j == height) || (j == height+1) || (i+j < height-1) )
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+}
