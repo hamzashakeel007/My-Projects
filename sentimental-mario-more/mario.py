375ec1eca1eda599179dd0c00c1de1d97d087c42
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-14 12:40:22 +0000

    Thu, Dec 14, 2023, 5:40 PM +05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,40 +1,29 @@
 # TODO
-#include <cs50.h>
-#include <stdio.h>
-
-void pyramid(void);
-
-int main(void)
-{
-    //Algorthm for building pyramid
-    pyramid();
-}
-
-
+import cs50
 
 void pyramid(void)
-{
+
+def main()
+    pyramid()
+
+
+
+def pyramid(void)
+
     // Pyramid height input
-    int height = 0;
+    height = 0
 
-    while ( (height < 1) || (height > 8) )
-    {
-        height = get_int("Enter height for mario's pyramid: \n");
-    }
+    while ( (height < 1) or (height > 8) ):
+        height = cs50.get_int("Enter height for mario's pyramid: \n")
 
-    for (int i = 0; i < height; ++i)
-    {
-        for (int j = 0; j < height+i+3; ++j)
-        {
-            if ( (j == height) || (j == height+1) || (i+j < height-1) )
-            {
-                printf(" ");
-            }
-            else
-            {
-                printf("#");
-            }
-        }
+    for (int i = 0; i < height; ++i):
+        for (int j = 0; j < height+i+3; ++j):
+            if ( (j == height) or (j == height+1) or (i+j < height-1) ):
+                printf(" ")
+            else:
+                printf("#")
         printf("\n");
-    }
-}
+
+
+main()
+
