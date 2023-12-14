7fb9d82e88d51f226431dc7d092fb1c70cf00c24
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-14 12:44:14 +0000

    Thu, Dec 14, 2023, 5:44 PM +05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -11,12 +11,12 @@ def pyramid()
     # Pyramid height input
     height = 0
 
-    while ( (height < 1) or (height > 8) ):
+    while (height < 1) or (height > 8):
         height = cs50.get_int("Enter height for mario's pyramid: \n")
 
     for i in height ++i:
-        for (int j = 0; j < height+i+3; ++j):
-            if ( (j == height) or (j == height+1) or (i+j < height-1) ):
+        for j in height+i+3; ++j:
+            if (j == height) or (j == height+1) or (i+j < height-1):
                 print(" ")
             else:
                 print("#")
