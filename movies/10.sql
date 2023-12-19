7f201d4f3a2b5f5e7557f2b91450fa8dd99a8538
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 20:44:49 +0000

    Wed, Dec 20, 2023, 1:44 AM +05:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,2 +1,2 @@
-SELECT name FROM people, directors, movies, ratings, WHERE people.id = directors.person_id AND directors.movie_id = movies.id AND
-movies.year = 2004
+SELECT DISTINCT name FROM people, directors, movies, ratings WHERE people.id = directors.person_id AND directors.movie_id = movies.id AND
+ratings.rating >= 9.0;
