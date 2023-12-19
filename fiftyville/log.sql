afd919be6fb79ee3d2e1fae737679ad0432d4039
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 22:16:45 +0000

    Wed, Dec 20, 2023, 3:16 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,4 +3,5 @@ SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND str
 -- according to crime scene report description we know to look for witnesses' transcripts from interviews
 SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 -- According to witness #1 (Ruth)   Eugene Raymond
-SELECT activity, license_plate, name FROM people, bakery_security_logs WHERE 
+SELECT activity, license_plate, name FROM people, bakery_security_logs WHERE people.license_plate = bakery_security_logs.license_plate
+AND 
