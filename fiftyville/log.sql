0e84ce21d19240ce0ec1613b61dac27449d3a172
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 22:38:29 +0000

    Wed, Dec 20, 2023, 3:38 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,7 +11,6 @@ AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 -- Suspects Vanessa (5P2BI95), Bruce (94KL13X), Barry (6P58WS2), Luca (4328GD8),
 -- Sofia (G412CB7), Iman (L93JTIZ), Diana (322W7JE), Kelsey (0NTHK55)
 -- According to witness #2 (Eugene)
-SELECT people.name, atm_transactions.transaction_type FROM people, bank_accounts, atm_transactions
-WHERE people.id = bank_accounts.person_id AND atm_transactions.account_number = bank_accounts.account_number
-AND atm_transactions.month = 7 AND atm_transactions.day = 28
-AND atm_transactions.transaction_type = 'withdraw' AND atm_location = 'Legett Street';
+SELECT name, transaction_type FROM people, bank_accounts, atm_transactions WHERE people.id = bank_accounts.person_id
+AND atm_transactions.account_number = bank_accounts.account_number AND atm_transactions.month = 7 AND atm_transactions.day = 28
+AND transaction_type = 'withdraw' AND atm_location = 'Leggett Street';
