9dccee124ea78b067c4c4309ef8b725cf3a1c6f6
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 21:41:02 +0000

    Wed, Dec 20, 2023, 2:41 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+-- according to crime scene report description we know to look for the time and activity from bakery security log
+
