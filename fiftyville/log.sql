f137ff0165f7f0081e59a31f9347cd40f8a586d8
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 21:52:03 +0000

    Wed, Dec 20, 2023, 2:52 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- according to crime scene report description we know to look for the time and activity, and license_plate from bakery security log
-SELECT activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND street = 'Humphrey Street' AND hour = 10 AND minute = 15;
+SELECT activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute = 15;
