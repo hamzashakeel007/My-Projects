3c01f2c863ebe63dcda41200c606005128fd4603
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 21:07:59 +0000

    Wed, Dec 20, 2023, 2:07 AM +05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,3 @@
 SELECT title FROM people, stars, movies WHERE people.id = stars.person_id AND stars.movie_id = movies.id
-AND (people.name = 'Bradley Cooper' OR 'Jennifer Lawrence');
+AND people.name = 'Bradley Cooper' UNION (SELECT title FROM people, stars, movies WHERE people.id = stars.person_id
+AND stars.movie_id = movies.id AND people.name = 'Jennifer Lawrence');
