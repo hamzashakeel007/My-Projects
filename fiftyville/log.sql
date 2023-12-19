c2ffdc2d1ee796cd4bd10e61f3b6d21a09f243cd
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 22:26:28 +0000

    Wed, Dec 20, 2023, 3:26 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+
 -- according to crime scene report description we know to look for witnesses' transcripts from interviews
 SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28 AND transcript LIKE '%bakery%';
+
 -- According to witness #1 (Ruth)   Eugene Raymond
 SELECT activity, people.license_plate, name FROM people, bakery_security_logs WHERE people.license_plate = bakery_security_logs.license_plate
 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
+
+-- Suspects Vanessa (5P2BI95), Bruce (94KL13X), Barry (6P58WS2), Luca (4328GD8),
+-- Sofia (G412CB7), Iman (L93JTIZ), Diana (322W7JE), Kelsey (0NTHK55)
+
