93deda2c356aa9e693ecd4da5b99a997377067bb
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 23:12:47 +0000

    Wed, Dec 20, 2023, 4:12 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,7 +4,7 @@ SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND str
 -- according to crime scene report description we know to look for witnesses' transcripts from interviews
 SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 
--- According to witness #1 (Ruth)   Eugene Raymond
+-- According to witness #1 (Ruth)
 SELECT activity, people.license_plate, name FROM people, bakery_security_logs WHERE people.license_plate = bakery_security_logs.license_plate
 AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 -- Suspects According to witness #1: Vanessa (5P2BI95), Bruce (94KL13X), Barry (6P58WS2), Luca (4328GD8),
@@ -16,7 +16,7 @@ AND atm_transactions.account_number = bank_accounts.account_number AND atm_trans
 AND transaction_type = 'withdraw' AND atm_location = 'Leggett Street';
 --Suspects According to witness #2: Bruce, Brooke, Diana, Kenny, Iman, Luca, Taylor, Benista
 
--- According to witness #2 (Eugene)
+-- According to witness #3 (Raymond)
 SELECT caller_name, receiver_name,caller, receiver FROM phone_calls WHERE month = 7 AND day = 28 AND duration <60;
 
 ALTER TABLE phone_calls ADD caller_name;
@@ -26,3 +26,4 @@ UPDATE phone_calls SET caller_name = people.name FROM people WHERE phone_calls.c
 UPDATE phone_calls SET receiver_name = people.name FROM people WHERE phone_calls.receiver = people.phone_number;
 
 SELECT caller_name, receiver_name,caller, receiver FROM phone_calls WHERE month = 7 AND day = 28 AND duration <60;
+--Suspects (Accomplices) sccording to witness #3: Sofia (Jack), Kelsey (Larry), Bruce (Robin), 
