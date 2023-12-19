dc422211db1ded766c6038d124ff747f36ce659f
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-19 22:02:07 +0000

    Wed, Dec 20, 2023, 3:02 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- according to crime scene report description we know to look for witnesses' transcripts from interviews
-SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28;
--- Suspects Ruth, Eugene, Raymond
+SELECT transcript FROM interviews WHERE month = 7 AND day = 28 AND transcript LIKE '%bakery%';
+
