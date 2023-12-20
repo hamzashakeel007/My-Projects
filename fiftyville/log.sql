16506453bf0aa859eadb2257c186e723c0271833
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-20 00:07:17 +0000

    Wed, Dec 20, 2023, 5:07 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -46,4 +46,4 @@ AND name IN (SELECT caller_name FROM phone_calls WHERE month = 7 AND day = 28 AN
 AND name IN (SELECT people.name FROM people WHERE people.id = bank_accounts.person_id AND atm_transactions.account_number = bank_accounts.account_number
 AND atm_transactions.month = 7 AND atm_transactions.day = 28 AND transaction_type = 'withdraw' AND atm_location = 'Leggett Street')
 AND name IN (SELECT people.name FROM people WHERE people.license_plate = bakery_security_logs.license_plate
-AND month = 7 AND bakery_security_logs.day = 28 AND bakery_security_logs.hour = 10 AND bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <= 25);
+AND bakery_security_logs.month = 7 AND bakery_security_logs.day = 28 AND bakery_security_logs.hour = 10 AND bakery_security_logs.minute >= 15 AND bakery_security_logs.minute <= 25);
