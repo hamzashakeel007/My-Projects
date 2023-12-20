d6c9c1c2e6a7e23b7fb6a89e1550705d646a37fa
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-20 00:00:40 +0000

    Wed, Dec 20, 2023, 5:00 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -40,9 +40,10 @@ WHERE people.passport_number = passengers.passport_number AND flights.id = passe
 ORDER BY flights.hour ASC;
 -- Final suspects Doris, Sofia, Bruce, Edward, Kelsey, Taylor, Kenny, Luna
 
-SELECT name FROM people, passengers, flights, phone_calls, bank_accounts, atm_transactions
+SELECT name FROM people, passengers, flights, phone_calls, bank_accounts, atm_transactions, bakery_security_logs
 WHERE (people.passport_number = passengers.passport_number AND flights.id = passengers.flight_id AND flights.id = 36)
 AND name IN (SELECT caller_name, FROM phone_calls WHERE month = 7 AND day = 28 AND duration <60)
 AND name IN (SELECT people.name WHERE people.id = bank_accounts.person_id AND atm_transactions.account_number = bank_accounts.account_number
 AND atm_transactions.month = 7 AND atm_transactions.day = 28 AND transaction_type = 'withdraw' AND atm_location = 'Leggett Street';)
-AND name IN (SELECT )
+AND name IN (SELECT name WHERE people.license_plate = bakery_security_logs.license_plate
+AND month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
