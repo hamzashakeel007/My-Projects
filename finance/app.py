e1ab8c90862f6b15818f0163bd0084a2b9bef2ef
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-27 16:40:40 +0000

    Wed, Dec 27, 2023, 8:40 AM -08:00

--- finance/app.py
+++ finance/app.py
@@ -135,16 +135,14 @@ def register():
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if len(rows) != 0:
+            return apology("username already exists", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
         return redirect("/")
-    return apology("TODO")
 
 
 @app.route("/sell", methods=["GET", "POST"])
