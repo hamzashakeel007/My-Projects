fcef1a6fbb98623f0d8ac598c642a5444a790456
hamzashakeel007 <150952042+hamzashakeel007@users.noreply.github.com>
2023-12-27 20:04:58 +0000

    Thu, Dec 28, 2023, 1:04 AM +05:00

--- finance/app.py
+++ finance/app.py
@@ -127,25 +127,25 @@ def register():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 200)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         # Ensure password confirmation was submitted
         elif not request.form.get("confirmpassword"):
-            return apology("must confirm password", 403)
+            return apology("must confirm password", 400)
 
         # Ensure password submission and confirmation matches
         elif request.form.get("password") != request.form.get("confirmation"):
-            return apology("Ensure that passwords match", 403)
+            return apology("Ensure that passwords match", 400)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         if len(rows) != 0:
-            return apology("username already exists", 403)
+            return apology("username already exists", 200)
 
         # insert new user into the database
         db.execute("INSERT INTO users (username, hash) VALUES(?,?)", request.form.get("username"),
